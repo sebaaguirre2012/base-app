339b251dda6e680fa5e8b25ac611f49a92439c2f
sebaaguirre2012 <sebaaguirre2012@gmail.com>
2023-09-04 22:38:00 -0300

    Add loading, toast and alert

--- src/app/pages/crud-item/crud-item.page.ts
+++ src/app/pages/crud-item/crud-item.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import { Item } from 'src/app/shared/item';
+import { LoadingController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
     selector: 'app-add-item',
@@ -12,39 +15,16 @@ export class CrudItemPage implements OnInit {
     item: Item;
     items: Item[] = [];
     enable: Boolean = false;
+    loading: any;
+    toast: any;
 
-    constructor(private database: FirestoreService) { }
+    constructor(private database: FirestoreService,
+        private loadingCtrl: LoadingController,
+        private toastController: ToastController,
+        private alertController: AlertController) { }
 
     ngOnInit() {
         this.getItems();
-    }
-
-    addItem() {
-        this.database.createDoc(this.item, 'items/', this.item.id)
-            .then(res => {
-     
-            }).catch(error => {
-                console.log('Error ->', error);
-            });
-    }
-
-    uploadImage(event: any) {
-        console.log(event);
-    }
-
-    openCamera() {
-        console.log('Camara');
-    }
-
-    getItems() {
-        this.database.getCollection<Item>('items/').subscribe(res => {
-            console.log(res);
-            this.items = res;
-        });
-    }
-
-    deleteItem(item: Item) {
-        this.database.deleteDoc('items/', item.id);
     }
 
     newItem() {
@@ -56,5 +36,72 @@ export class CrudItemPage implements OnInit {
             date: new Date,
             photo: ''
         }
+    }
+
+    addItem() {
+        this.showLoading('Adding Item...');
+        this.database.createDoc(this.item, 'items/', this.item.id)
+            .then(res => {
+                this.loading.dismiss();
+                this.presentToast('Item Added...');
+            }).catch(error => {
+                console.log('Error ->', error);
+            });
+    }
+
+    getItems() {
+        this.database.getCollection<Item>('items/').subscribe(res => {
+            this.items = res;
+        });
+    }
+
+    async deleteItem(item: Item) {
+        const alert = await this.alertController.create({
+            header: 'Delete',
+            message: 'Are you sure?',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: () => {
+                        console.log('Cancel');;
+                    },
+                },
+                {
+                    text: 'OK',
+                    role: 'confirm',
+                    handler: () => {
+                        this.showLoading('Removing Item...');
+                        this.database.deleteDoc('items/', item.id)
+                            .then(res => {
+                                this.loading.dismiss();
+                                this.presentToast('Item Deleted...');
+                            }).catch(error => {
+                                console.log('Error ->', error);
+                            });
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
+    }
+
+    uploadImage(event: any) {
+        console.log(event);
+    }
+
+    openCamera() {
+        console.log('Camara');
+    }
+
+    async showLoading(message: string) {
+        this.loading = await this.loadingCtrl.create({ message });
+        this.loading.present();
+    }
+
+    async presentToast(message: string) {
+        this.toast = await this.toastController.create({ message, duration: 3000 });
+        await this.toast.present();
     }
 }
