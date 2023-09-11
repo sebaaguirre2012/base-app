0f0931c0fcff8ad16505c585aed0f6b012373c16
Sebastian Aguirre <saguirre.knh@veritran.com>
2023-09-11 20:30:29 -0300

    Add resolucion de conflictos

---
+++ android/app/src/main/java/com/sna/baseapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sna.baseapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDok-GJ2rZjc-J8Axetd-YtEmrNiDHI2xY",
+    authDomain: "pps-2023-d07fd.firebaseapp.com",
+    projectId: "pps-2023-d07fd",
+    storageBucket: "pps-2023-d07fd.appspot.com",
+    messagingSenderId: "797960400433",
+    appId: "1:797960400433:web:5fcef1d0b107d54713ef10",
+    measurementId: "G-2MFWYZD8YS"
+  }
 };
